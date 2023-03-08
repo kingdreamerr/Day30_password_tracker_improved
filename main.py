d8e9d07b2c79866ea4e9b6533b368addaca9f6d6
kingdreamerr <jackmvula458@gmail.com>
2023-03-08 21:10:14 +0200

    Position btn

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ password_label.grid(row=3, column=0)
 
 
 #Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2,pady=5)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2,pady=5)
