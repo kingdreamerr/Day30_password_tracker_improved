0a7b4e0ea2033fd479358792e5b114482d89c248
kingdreamerr <jackmvula458@gmail.com>
2023-03-08 21:20:20 +0200

    Check to see if the file is there or not

--- main.py
+++ main.py
@@ -50,11 +50,12 @@ def save():
         }
     }
 
-    if len(website) == 0 or len(password) == 0:
+   if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
-                                                      f"\nPassword: {password} \nIs it ok to save?")
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
         if is_ok:
             with open("data.txt", "a") as data_file:
                 data_file.write(f"{website} | {email} | {password}\n")
